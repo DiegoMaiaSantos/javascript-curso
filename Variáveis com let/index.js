abbd214915fb2cefd071dfbd233fcad0033b1571
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-09-14 23:43:53 -0300

    Add index.js da aula de let.

---
+++ Variáveis com let/index.js
@@ -0,0 +1,29 @@
+let nome = 'João' ; // Declarou a variável e inicou.
+
+console.log(nome, 'nasceu em 1984.')
+console.log('Em 2000', nome, 'conheceu Maria.')
+console.log(nome, 'casou-se com Maria em 2012.')
+console.log('Maria teve 1 filho com', nome, 'em 2015.')
+console.log('O filho de', nome, 'se chama Edurado.')
+
+// Outro exemplo.
+
+nome = 'João'; //Inicializei a variável.
+console.log(nome);
+nome = 'Diego';
+console.log(nome);
+
+/**Regras para criar as variáveis:
+ * Variáveis precisam ter nomes significativos;
+ * Não pode começar o nome de uma variável com número;
+ * Não pode conter espaços ou traços;
+ * Utilizamos camelCase;
+ * Case-sensitive;
+ * Não podemos redeclarar variáveis com let;
+ * Não utilize var, utilize let.
+ */
+
+let nomeCliente = 'Diego';
+let nomecliente = 'Santos';
+
+console.log(nomeCliente, nomecliente);
