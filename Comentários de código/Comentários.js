864770b17ed1a4155e84e775f55450fac6c2872e
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-09-15 23:59:42 -0300

    Create Comentários.js

---
+++ Comentários de código/Comentários.js
@@ -0,0 +1,19 @@
+// E escrever um comentário
+console.log('Olá mundo!'); //Aqui tem outro comentário
+console.log(); //Com isso eu consigo colocar um espaço de um texto para o outro.
+console.log('Linha 1');
+//Comentário
+
+/* Um bloco de código 
+Linha de comentário*/
+
+/**
+ * Um título
+ * 
+ * Lorem ipsum
+ */
+
+// console.log('Linha 2');
+console.log('Linha 3');
+console.log('Linha 4');
+// Vai ser ignorado
