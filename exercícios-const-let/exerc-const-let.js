b960c170268f9ee7a8150d0bcf639079e3adfff3
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-09-29 23:28:28 -0300

    Resolução do exercício.

--- exercícios-const-let/exerc-const-let.js
+++ exercícios-const-let/exerc-const-let.js
@@ -1,11 +1,15 @@
 /* Luiz Otávio Miranda tem 30 anos, pesa 84 kg
 tem 1.80 de altura e seu IMC é de 25.92592592525924
+Luiz Otávio nasceu em 1980
 */
 
 const nome = 'Diego Maia';
 const sobrenome = 'Santos';
 const idade = 31;
-const peso = 70;
+const peso = 69;
 const altura = 1.75;
+let imc = peso / (altura * altura); // peso / (altura * altura)
+let anoNascimento = 2021 - idade;
 
-let imc; // peso / (altura * altura)
+console.log(imc)
+console.log(anoNascimento)
