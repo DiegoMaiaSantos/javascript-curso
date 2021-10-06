5b49f7b70f0ae3fe37dc50e37076bbd5789c8cde
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-10-05 22:09:37 -0300

    Atualização

--- diferenca-let-var/let-var.js
+++ diferenca-let-var/let-var.js
@@ -1,5 +1,10 @@
-// ECMAScript 2015 (ES6)
+// ECMAScript 2015 (ES6) JavaScript
+
 var nome = 'Luiz';
 var nome = 'Otávio'
 
 console.log(nome);
+
+nome01 = 'Diego'; // Não faça isso!
+
+console.log(nome01)
