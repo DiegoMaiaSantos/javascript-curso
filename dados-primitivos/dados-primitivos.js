32726f30cf0ed220634ab56086ce03787fe8e468
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-10-10 19:56:09 -0300

    Atualização.

---
+++ dados-primitivos/dados-primitivos.js
@@ -0,0 +1,8 @@
+const nome = 'Diego';
+const nome1 = "Diego"; // Os 3 exemplos são strings.
+const nome2 = `Diego`;
+
+const num = 10;
+const num1 = 10.52; // Os dois exemplos são number.
+
+let nomeAluno; // undefined = não aponta para local nenhum na memória.
