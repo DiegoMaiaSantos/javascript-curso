c8ca100972709cbcc1ac266cca5dc9ccd3d5989a
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-10-10 21:42:25 -0300

    Atualização.

--- operadores-aritméticos-atribuicao-incremento/operadores-aritmeticos.js
+++ operadores-aritméticos-atribuicao-incremento/operadores-aritmeticos.js
@@ -1,9 +1,26 @@
 /**
  * Aritméticos
- * + Adição / Concatenação
- * - / *
+ * + -> Adição / Concatenação
+ * - / * -> Subtração, divisão e multiplicação.
+ * ** -> Potenciação
+ * % -> Resto da divisão
+ * 
+ * Sempre lembrar da ordem das precedências.
+ * Ordem:
+ * () -> Tudo que estiver dentro do parêntese vai ser executado primeiro.
+ * ** -> Depois potenciação.
+ * * / % -> Depois vem multiplicação, divisão e o resto da divisão.
+ * + - -> Depois mais e menos.
  * */
 
 const num1 = 5;
-const num2 = 10;
-console.log(num1 + num2);
+const num2 = 2;
+const num3 = 10
+console.log((num1 + num2) * num3);
+
+let contador = 1;
+contador++; // 2
+contador++; // 3
+contador++; // 4
+contador++; // 5
+console.log(contador);
