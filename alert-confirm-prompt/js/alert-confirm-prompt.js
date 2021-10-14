9efa1f505999e7ed94855e1b52acabb2c61859b3
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-10-13 23:27:21 -0300

    Informação adicional.

--- alert-confirm-prompt/js/alert-confirm-prompt.js
+++ alert-confirm-prompt/js/alert-confirm-prompt.js
@@ -8,3 +8,6 @@ num2 = Number(num2);
 const resul = num1 + num2;
 
 alert(`A soma dos dois números foi: ${resul}`);
+
+// Poderia colocar a expressão de soma dentro do ${num1 + num2}.
+// Mas a melhor maneira é criando uma variável.
