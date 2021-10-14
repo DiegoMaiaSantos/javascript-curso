6d0a836d1cc22560b7e475e843db95af2c89e645
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-10-13 23:15:52 -0300

    Aula finalizada + exercício.

--- alert-confirm-prompt/js/alert-confirm-prompt.js
+++ alert-confirm-prompt/js/alert-confirm-prompt.js
@@ -1,1 +1,10 @@
-alert('Uma mensagem aqui!')
+
+// Exercício:
+
+let num1 = prompt('Digite um número:');
+let num2 = prompt('Digite um número:');
+num1 = Number(num1);
+num2 = Number(num2);
+const num3 = num1 + num2;
+
+alert(`A soma dos dois números é: ${num3}`);
