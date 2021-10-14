6bfce0a70081eea5200ddbee89f151f6c9489033
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-10-13 23:21:48 -0300

    Modificações.

--- alert-confirm-prompt/js/alert-confirm-prompt.js
+++ alert-confirm-prompt/js/alert-confirm-prompt.js
@@ -5,6 +5,5 @@ let num1 = prompt('Digite um número:');
 let num2 = prompt('Digite um número:');
 num1 = Number(num1);
 num2 = Number(num2);
-const num3 = num1 + num2;
-
-alert(`A soma dos dois números é: ${num3}`);
+const resul = num1 + num2;
+alert(`A soma dos dois números foi: ${resul}`);
