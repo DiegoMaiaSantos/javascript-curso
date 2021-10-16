3856b65e97e4b21a4f99e0dc1afc969354670f5d
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-10-16 15:59:13 -0300

    Conteúdo da aula Objeto Math.

---
+++ objeto-math/objeto-math.js
@@ -0,0 +1,13 @@
+// Aula Objeto Math
+
+let num1 = 9.54578;
+let num2 = Math.floor(num1); // Arredonda para baixo.
+console.log(num2);
+
+let num3 = 9.54578;
+let num4 = Math.ceil(num3); // Arredonda para cima.
+console.log(num4);
+
+let num5 = 9.54578;
+let num6 = Math.round(num5); // Arredonda para o número mais próximo acima de 50 ou abaixo.
+console.log(num6);
