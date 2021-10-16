a7374177e40ab803d1c60555d54e1018c57ff4c2
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-10-16 09:57:39 -0300

    Dica para resolução simples.

--- exercício-variáveis/exerc-var.js
+++ exercício-variáveis/exerc-var.js
@@ -4,6 +4,8 @@ let varC = 'C'; // A
 
 let varD = 'D'; // Vai servir para receber outra variável, para organizar do jeito que preciso.
 
+// [varA, varB, varC] = [varB, varC, varA] -> A maneira mais fácil de resolver o exercício.
+
 varD = varA
 varA = varB;
 varB = varC;
