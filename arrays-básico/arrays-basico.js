b97a9828f5d0f8353f49e1902909c767e7ce122c
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-10-23 22:20:16 -0300

    Conteúdo da aula.

--- arrays-básico/arrays-basico.js
+++ arrays-básico/arrays-basico.js
@@ -1,4 +1,24 @@
 // Arrays (básico):
+
 //               0       1        2
 const alunos = ['Luiz', 'Maria', 'João'];
+
 console.log(alunos);
+
+// alunos.unshift('Luiza'); // Inclui no começo.
+// alunos.unshift('Fábio');// O que vem depois vai ser sempre o primeiro.
+
+
+// alunos.push('Luiza'); // Com essa função inclui no final.
+// alunos.push('Fábio');
+
+// alunos[alunos.length] = 'Luiza'; // Vai ser incluido um novo valor pois o nome não existe. No final.
+// alunos[alunos.length] = 'Fábio'; // Vai ser incluido um novo valor pois o nome não existe.
+// alunos[alunos.length] = 'Luana'; // Vai ser incluido um novo valor pois o nome não existe.
+
+//alunos[0] = 'Eduardo'; // Muda o número especificado.
+//alunos[3] = 'Luiza'; // Inclui mais um nome.
+
+//console.log(alunos);
+//console.log(alunos[0]);
+//console.log(alunos[2]);
