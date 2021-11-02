e24c2180acddbd9b9116bdfceae1fc54813293da
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-11-02 20:46:58 -0300

    Aula de objetos (básico).

---
+++ objetos-básico/objetos-basico.js
@@ -0,0 +1,16 @@
+
+function criaPessoa(nome, sobrenome, idade) {
+    return {
+        nome: nome,
+        sobrenome: sobrenome,
+        idade: idade
+    };
+}
+
+const pessoa1 = criaPessoa('Luiz', 'Miranda', 25);
+const pessoa2 = criaPessoa('Maria', 'Oliveira', 32);
+const pessoa3 = criaPessoa('João', 'Moreira', 55);
+const pessoa4 = criaPessoa('Junior', 'Filho', 44);
+const pessoa5 = criaPessoa('Jean', 'Otávio', 69);
+
+console.log(pessoa1, pessoa2, pessoa3, pessoa4, pessoa5);
