634c44fafa257989aa5d37a8f6ef6b99f61e64a3
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-11-09 22:14:02 -0300

    Aula finalizada.

--- valores-primitivos-referencia/valores-primi-refe.js
+++ valores-primitivos-referencia/valores-primi-refe.js
@@ -1,13 +1,25 @@
 /* 
-Primitivos (imutáveis) - string, number, boolean, undefined, null (bigint, symbol)
+Primitivos (imutáveis) - string, number, boolean, undefined,
+null (bigint, symbol) - Valores copiados.
 
-Referência (mutável) - array, object, function
+Referência (mutável) - array, object, function - Passados por referência.
 */
 
 // ------------- Referência (mutáveis) ------------------
+const a1 = {
+    nome: 'Luiz',
+    sobrenome: 'Otávio'
+};
+const b1 = a1;
+
+a1.nome = 'João';
+console.log(a1);
+console.log(b1);
+
 
 let a = [1, 2, 3];
-let b = a;
+let b = [...a];
+let c = b;
 console.log(a, b);
 
 a.push(4);
@@ -15,6 +27,9 @@ console.log(a, b);
 
 b.pop();
 console.log(b, a);
+
+a.push('Luiz');
+console.log(c);
 
 // ------------- Primitivos (imutáveis) ------------------
 
