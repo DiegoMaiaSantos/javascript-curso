44844a8efeadd0694602f367bb18db37da33dd35
DiegoMaiaSantos <diegom.santos03@gmail.com>
2021-11-19 23:39:01 -0300

    Novo conteúdo da aula.

--- exerc-funcao-array-objetos/js/exercicio.js
+++ exerc-funcao-array-objetos/js/exercicio.js
@@ -1,1 +1,16 @@
 // Exercício com funções, array e objetos.
+
+function meuEscopo() {
+    const form = document.querySelector('.form');
+
+    function recebeEventoForm(evento) {
+        evento.preventDefault();
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
+}
+meuEscopo();
